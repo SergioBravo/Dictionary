8816130fe6946db1fa3e30d932949c732e33f08c
Serhii Pozhydaiev <revers.xz@gmail.com>
2016-06-06 10:28:02 +0300

    Добавлены файлы проекта

---
+++ MainActivity.java
@@ -0,0 +1,75 @@
+package com.test.sergio.dictionary;
+
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    Button bSearch;
+    EditText editText;
+    TextView recentlySearched;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        editText = (EditText) findViewById(R.id.editText);
+
+        recentlySearched = (TextView) findViewById(R.id.recentlySearched);
+
+        bSearch = (Button) findViewById(R.id.bSearch);
+        bSearch.setOnClickListener(this);
+    }
+
+
+    public void onClick(View v) {
+        String strWord = editText.getText().toString();
+        if (recentlySearched.getText().toString() == "") {
+            recentlySearched.setText(strWord + "\n");
+        } else {
+            recentlySearched.append(strWord + "\n");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
